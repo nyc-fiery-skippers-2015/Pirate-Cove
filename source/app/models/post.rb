de72e0ecf27488b180fe42b52b155726bfacf223
Samantha Belkin <samantha.belkin@gmail.com>
2015-05-22 14:18:07 -0400

    add model and migration

--- source/app/models/post.rb
+++ source/app/models/post.rb
@@ -1,3 +1,7 @@
 class Post < ActiveRecord::Base
-  # Remember to create a migration!
+  validates :title, :presence => true
+  validates :body, :presence => true
+
+  has_and_belongs_to_many :tags
+  belongs_to :author, class_name: 'User'
 end
