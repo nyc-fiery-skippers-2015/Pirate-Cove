7e8df73f237de6f3def9bd7c874d09cc94a0280a
manentea <manentea@gmail.com>
2015-05-22 14:20:22 -0400

    added groups migrations and associations

--- source/db/migrate/20150522121406_create_groups.rb
+++ source/db/migrate/20150522121406_create_groups.rb
@@ -1,4 +1,9 @@
 class CreateGroups < ActiveRecord::Migration
   def change
+    create_table :groups do |t|
+      t.string :name, null: false
+      t.references :owner, null: false
+      t.timestamps null: false
+    end
   end
 end
