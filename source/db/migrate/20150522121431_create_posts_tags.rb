3a79b0757b30a54c7c3e70c7bda612a68ec970e3
Mendel Schneerson <mmschneerson@gmail.com>
2015-05-22 14:29:30 -0400

    added migration ; e'

--- source/db/migrate/20150522121431_create_posts_tags.rb
+++ source/db/migrate/20150522121431_create_posts_tags.rb
@@ -1,4 +1,8 @@
 class CreatePostsTags < ActiveRecord::Migration
   def change
+    create_table :posts_tags do |t|
+      t.belongs_to :posts, null: false
+      t.belongs_to :tags, null: false
+    end
   end
 end
