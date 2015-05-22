770cfc687be57bfc256ea1f11241a848b7127f17
JMC11 & Nikolai Calder <pair+JMC11.nikocal@devbootcamp.com>
2015-05-22 14:26:06 -0400

    corrected name for association

--- source/app/models/user.rb
+++ source/app/models/user.rb
@@ -1,4 +1,4 @@
 class User < ActiveRecord::Base
   has_many :posts, foreign_key: 'author_id'
-  has_and_belongs_to :groups, foreign_key: 'owner_id'
+  has_and_belongs_to_many :groups, foreign_key: 'owner_id'
 end
